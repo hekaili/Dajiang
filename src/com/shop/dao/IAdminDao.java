e43908634a73ce1d6cb34b57c50f86b499d1847f
hekaili <17638431864@163.com>
2019-01-14 16:26:24 +0800

    dier commit.

--- src/com/shop/dao/IAdminDao.java
+++
@@ -1,8 +0,0 @@
-package com.shop.dao;
-
-import com.shop.model.Admin;
-
-public interface IAdminDao {
-	//验证后台超级管理员登录
-	public Admin find (String adminName, String adminPwd);	
-}
--- src/com/shop/service/IAdminService.java
+++
@@ -1,8 +0,0 @@
-package com.shop.service;
-
-import com.shop.model.Admin;
-
-public interface IAdminService {
-	//检验后台超级管理员登录
-	public Admin checkAdminService(String adminName, String adminPwd);
-}
